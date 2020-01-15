142bb2a3f19e65ae499a91abd19dbc9b1b89b47f
Evelina Bakhturina <ebakhturina@nvidia.com>
2020-01-15 12:12:33 -0800

    sgd datalayer added, test passed
    Signed-off-by: Evelina Bakhturina <ebakhturina@nvidia.com>

--- collections/nemo_nlp/nemo_nlp/data/datasets/sgd/data_utils.py
+++ collections/nemo_nlp/nemo_nlp/data/datasets/sgd/data_utils.py
@@ -4,6 +4,9 @@ import re
 import collections
 
 from nemo.utils.exp_logging import get_logger
+from nemo_nlp.data.datasets.sgd import tokenization
+
+import schema
 
 # Dimension of the embedding for intents, slots and categorical slot values in
 # the schema. Should be equal to BERT's hidden_size.
@@ -42,12 +45,11 @@ FILE_RANGES = {
 		"dev": range(1, 3),
 		"test": range(1, 3)
 	}
-	# "dstc8_all": {
-	# 	"train": range(1, 128),
-	# 	"dev": range(1, 21),
-	# 	"test": range(1, 35)
-	# }
 }
+
+# Name of the file containing all predictions and their corresponding frame
+# metrics.
+PER_FRAME_OUTPUT_FILENAME = "dialogues_and_metrics.json"
 
 DATABASE_EXISTS_TMP = '{} dataset has already been processed and stored at {}'
 MODE_EXISTS_TMP = \
@@ -208,29 +210,31 @@ class Dstc8DataProcessor(object):
 	"""Data generator for dstc8 dialogues."""
 
 	def __init__(self,
-				 dstc8_data_dir,
-				 train_file_range,
-				 dev_file_range,
-				 test_file_range,
-				 tokenizer,
-				 max_seq_length=DEFAULT_MAX_SEQ_LENGTH,
-				 log_data_warnings=False):
-		self.dstc8_data_dir = dstc8_data_dir
-		self._log_data_warnings = log_data_warnings
-		self._file_ranges = {
-			"train": train_file_range,
-			"dev": dev_file_range,
-			"test": test_file_range,
-		}
-		# BERT tokenizer
-		self._tokenizer = tokenizer
-		self._max_seq_length = max_seq_length
+	               dstc8_data_dir,
+	               train_file_range,
+	               dev_file_range,
+	               test_file_range,
+	               vocab_file,
+	               do_lower_case,
+	               max_seq_length=DEFAULT_MAX_SEQ_LENGTH,
+	               log_data_warnings=False):
+	    self.dstc8_data_dir = dstc8_data_dir
+	    self._log_data_warnings = log_data_warnings
+	    self._file_ranges = {
+	        "train": train_file_range,
+	        "dev": dev_file_range,
+	        "test": test_file_range,
+	    }
+	    # BERT tokenizer
+	    self._tokenizer = tokenization.FullTokenizer(
+	        vocab_file=vocab_file, do_lower_case=do_lower_case)
+	    self._max_seq_length = max_seq_length
 
 	def get_dialog_examples(self, dataset):
 		"""Return a list of `InputExample`s of the data splits' dialogues.
 
 		Args:
-		  dataset: str. can be "train", "dev", or "test".
+		  dataset(str): can be "train", "dev", or "test".
 
 		Returns:
 		  examples: a list of `InputExample`s.
@@ -242,7 +246,7 @@ class Dstc8DataProcessor(object):
 		]
 		dialogs = load_dialogues(dialog_paths)
 		schema_path = os.path.join(self.dstc8_data_dir, dataset, "schema.json")
-		schemas = Schema(schema_path)
+		schemas = schema.Schema(schema_path)
 
 		examples = []
 		for dialog_idx, dialog in enumerate(dialogs):
@@ -426,133 +430,6 @@ class Dstc8DataProcessor(object):
 					example_count += len(turn["frames"])
 		return example_count
 
-
-class ServiceSchema(object):
-	"""A wrapper for schema for a service."""
-
-	def __init__(self, schema_json, service_id=None):
-		self._service_name = schema_json["service_name"]
-		self._description = schema_json["description"]
-		self._schema_json = schema_json
-		self._service_id = service_id
-
-		# Construct the vocabulary for intents, slots, categorical slots,
-		# non-categorical slots and categorical slot values. These vocabs are used
-		# for generating indices for their embedding matrix.
-		self._intents = sorted(i["name"] for i in schema_json["intents"])
-		self._slots = sorted(s["name"] for s in schema_json["slots"])
-		self._categorical_slots = sorted(
-			s["name"]
-			for s in schema_json["slots"]
-			if s["is_categorical"] and s["name"] in self.state_slots)
-		self._non_categorical_slots = sorted(
-			s["name"]
-			for s in schema_json["slots"]
-			if not s["is_categorical"] and s["name"] in self.state_slots)
-		slot_schemas = {s["name"]: s for s in schema_json["slots"]}
-		categorical_slot_values = {}
-		categorical_slot_value_ids = {}
-		for slot in self._categorical_slots:
-			slot_schema = slot_schemas[slot]
-			values = sorted(slot_schema["possible_values"])
-			categorical_slot_values[slot] = values
-			value_ids = {value: idx for idx, value in enumerate(values)}
-			categorical_slot_value_ids[slot] = value_ids
-		self._categorical_slot_values = categorical_slot_values
-		self._categorical_slot_value_ids = categorical_slot_value_ids
-
-	@property
-	def schema_json(self):
-		return self._schema_json
-
-	@property
-	def state_slots(self):
-		"""Set of slots which are permitted to be in the dialogue state."""
-		state_slots = set()
-		for intent in self._schema_json["intents"]:
-			state_slots.update(intent["required_slots"])
-			state_slots.update(intent["optional_slots"])
-		return state_slots
-
-	@property
-	def service_name(self):
-		return self._service_name
-
-	@property
-	def service_id(self):
-		return self._service_id
-
-	@property
-	def description(self):
-		return self._description
-
-	@property
-	def slots(self):
-		return self._slots
-
-	@property
-	def intents(self):
-		return self._intents
-
-	@property
-	def categorical_slots(self):
-		return self._categorical_slots
-
-	@property
-	def non_categorical_slots(self):
-		return self._non_categorical_slots
-
-	def get_categorical_slot_values(self, slot):
-		return self._categorical_slot_values[slot]
-
-	def get_slot_from_id(self, slot_id):
-		return self._slots[slot_id]
-
-	def get_intent_from_id(self, intent_id):
-		return self._intents[intent_id]
-
-	def get_categorical_slot_from_id(self, slot_id):
-		return self._categorical_slots[slot_id]
-
-	def get_non_categorical_slot_from_id(self, slot_id):
-		return self._non_categorical_slots[slot_id]
-
-	def get_categorical_slot_value_from_id(self, slot_id, value_id):
-		slot = self.categorical_slots[slot_id]
-		return self._categorical_slot_values[slot][value_id]
-
-	def get_categorical_slot_value_id(self, slot, value):
-		return self._categorical_slot_value_ids[slot][value]
-
-
-class Schema(object):
-	"""Wrapper for schemas for all services in a dataset."""
-
-	def __init__(self, schema_json_path):
-		# Load the schema from the json file.
-		with open(schema_json_path, "r") as f:
-			schemas = json.load(f)
-		self._services = sorted(schema["service_name"] for schema in schemas)
-		self._services_vocab = {v: k for k, v in enumerate(self._services)}
-		service_schemas = {}
-		for schema in schemas:
-			service = schema["service_name"]
-			service_schemas[service] = ServiceSchema(
-				schema, service_id=self.get_service_id(service))
-		self._service_schemas = service_schemas
-
-	def get_service_id(self, service):
-		return self._services_vocab[service]
-
-	def get_service_from_id(self, service_id):
-		return self._services[service_id]
-
-	def get_service_schema(self, service):
-		return self._service_schemas[service]
-
-	@property
-	def services(self):
-		return self._services
 
 
 class InputExample(object):
